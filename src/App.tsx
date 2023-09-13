8c5ae348012b9e4ff49f0da9efaa7e149155fde8
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-13 16:07:34 -0300

    'label'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Github } from "lucide-react";
+import { Github, FileVideo } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
@@ -28,12 +29,12 @@ export function App() {
           <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Inclua o prompt para a IA..." 
+              placeholder="Inclua o prompt para a IA..."
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
-              reaOnly
+              readOnly
             />
           </div>
 
@@ -41,7 +42,31 @@ export function App() {
             Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
           </p>
         </div>
-        <aside className="w-80"></aside>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4 " />
+              Selecione um vídeo
+            </label>
+
+            <input type="file" id="video" accept="video/mp4" className="sr-only" />
+
+            <Separator />
+
+            <div className="space-y-1">
+              <Label htmlFor="transcription_prompt">Prompt de transcrção</Label>
+              <Textarea
+                id="transcription_prompt"
+                className="min-h-20 leading-relaxed" 
+                placeholder="Inclua palavras-chave mencionadas no vídeo separados por vírgula (,)"
+                />
+            </div>
+          </form>
+        </aside>
       </main>
     </div>
   )
