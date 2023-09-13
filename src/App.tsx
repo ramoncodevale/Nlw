d279c2cbb77e91192d973da81c191a6f6321dc80
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-13 16:30:13 -0300

    'select'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Github, FileVideo } from "lucide-react";
+import { Github, FileVideo, Upload } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 
 export function App() {
   return (
@@ -57,14 +58,39 @@ export function App() {
 
             <Separator />
 
-            <div className="space-y-1">
+            <div className="space-y-2">
               <Label htmlFor="transcription_prompt">Prompt de transcrção</Label>
               <Textarea
                 id="transcription_prompt"
-                className="min-h-20 leading-relaxed" 
+                className="h-20 leading-relaxed resize-none"
                 placeholder="Inclua palavras-chave mencionadas no vídeo separados por vírgula (,)"
-                />
+              />
             </div>
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+            <Separator />
           </form>
         </aside>
       </main>
