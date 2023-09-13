620f0d5bcb12243bf3b28c9d351df20e7bb2bab8
ramoncodevale <ramonvalentim88@gmail.com>
2023-09-13 16:37:56 -0300

    'slider'

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
 export function App() {
   return (
@@ -91,6 +92,20 @@ export function App() {
               </span>
             </div>
             <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider 
+               min={0}
+               max={1}
+               step={0.1}
+              />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixr o resultado mais criativo e com possíveos erros.
+              </span>
+            </div>
+       
+          <Separator />
           </form>
         </aside>
       </main>
